3b66b978868693ceb3bf4ea93d8df58abb1989f4
tianxb <m18614056677 1@163.com>
2018-12-14 22:42:08 +0800

    初始化项目

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,44 @@
+package com.baizhi.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private  String id;
+    private  Integer age;
+    private  String name;
+
+    public User() {
+        super();
+    }
+
+    public User(String id, Integer age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
